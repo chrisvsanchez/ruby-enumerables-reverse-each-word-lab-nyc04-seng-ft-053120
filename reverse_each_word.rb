3ed365cefab8f876a6143fe0e6dbf745fbf7af2b
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-09 22:47:47 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,16 +4,17 @@ split_sentence = sentence.split(" ")
 reversed_sentence = []
 split_sentence.each do |word|
   reversed_sentence << word.reverse
+  binding.pry
 end
 reversed_sentence.join(" ")
-binding.pry
-
+  binding.pry
 end
 
 def reverse_each_word(sentence)
 split_sentence = sentence.split(" ")
 reversed_sentence = []
 split_sentence.collect do |word|
+
   reversed_sentence << word.reverse
 end
 reversed_sentence.join(" ")
