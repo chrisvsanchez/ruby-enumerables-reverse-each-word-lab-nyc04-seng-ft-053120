74f58a4521db01a8f2749fdef037de6a25c99022
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-19 23:40:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -20,7 +20,7 @@ require 'pry'
 # reversed_sentence.join(" ")
 # end
 def reverse_each_word(sentence)
-reversed =  sentence.map{|letter| letter.reverse_each_word}
+reversed =  sentence.map{|letter| letter.reverse}
 binding.pry 
 reversed
 end
