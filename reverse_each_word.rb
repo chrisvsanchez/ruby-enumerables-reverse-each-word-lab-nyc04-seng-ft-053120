3b11a9ad14fdd88942c738801a1ef0bfaeb317e2
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-19 23:38:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,21 @@
 require 'pry'
-def reverse_each_word(sentence)
-split_sentence = sentence.split(" ")
-reversed_sentence = []
-split_sentence.each do |word|
-  reversed_sentence << word.reverse
-  binding.pry
-end
-reversed_sentence.join(" ")
-  binding.pry
-end
+# def reverse_each_word(sentence)
+# split_sentence = sentence.split(" ")
+# reversed_sentence = []
+# split_sentence.each do |word|
+#   reversed_sentence << word.reverse
+#   binding.pry
+# end
+# reversed_sentence.join(" ")
+#   binding.pry
+# end
 
-def reverse_each_word(sentence)
-split_sentence = sentence.split(" ")
-reversed_sentence = []
-split_sentence.collect do |word|
+# def reverse_each_word(sentence)
+# split_sentence = sentence.split(" ")
+# reversed_sentence = []
+# split_sentence.collect do |word|
 
-  reversed_sentence << word.reverse
-end
-reversed_sentence.join(" ")
-end
+#   reversed_sentence << word.reverse
+# end
+# reversed_sentence.join(" ")
+# end
